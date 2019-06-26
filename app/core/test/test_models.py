144d9e3811d786e5a2010d998bde2395b458cc1b
Lincoin <linchnx@163.com>
2019-06-25 20:35:12 -0400

    fix bug

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
-def sample_user(email='test@londonappdev.com', password='testpass'):
+def sample_user(email='test@gmail.com', password='testpass'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
 
@@ -45,7 +45,7 @@ class ModelTests(TestCase):
 
     def test_tag_str(self):
         """Test the tag string representation"""
-        tag = models.Tags.objects.create(
+        tag = models.Tag.objects.create(
             user=sample_user(),
             name='Vegan'
         )
