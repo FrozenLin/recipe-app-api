8f6fa1132785f110056a188c23c5ea432bbe9d77
fightinglinc <linchnx@163.com>
2019-06-26 09:16:57 -0400

    Add filter

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -223,7 +223,7 @@ class RecipeImageUploadTests(TestCase):
         """Test uploading an image to recipe"""
         url = image_upload_url(self.recipe.id)
         with tempfile.NamedTemporaryFile(suffix='.jpg') as ntf:
-            img = Image.new('RGB', 10, 10)
+            img = Image.new('RGB', (10, 10))
             img.save(ntf, format='JPEG')
             ntf.seek(0)
             res = self.client.post(url, {'image': ntf}, format='multipart')
